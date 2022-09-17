529605e468cf71b85c06fe8b459ab144020fe169
Shreeraj Palande <shreerajpalande@gmail.com>
2022-09-17 20:53:01 +0530

    Added reviews and home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import {Container} from "react-bootstrap"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./views/Home";
 
 const App= () => {
   return (
     <>
     <Header/>
-    <main className="py-3">
-      <Container>
-      <h1>Welcome to EShop</h1>
-      </Container>
-      
-    </main>
+      <main className="py-3">
+        <Container>
+          <Home/>
+        </Container> 
+      </main>
     <Footer/>
     </>
   );
---
+++ frontend/src/views/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import Product from "../components/Product"
+import products from "../products"
+
+const Home = () => {
+  return (
+    <>
+    <h1>
+        <Row>
+                {products.map(product=>(
+                  <Col key={product._id} sm="12" md="6" lg="4" xl="3">
+                    <Product product={product} />
+                  </Col>   
+                ))}
+        </Row>
+    </h1> 
+    </>
+  )
+}
+
+export default Home
